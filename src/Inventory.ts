8e58a055eadef3310da7d32b686364001f2ff1ff
FriskyXE <supanat.tinz@gmail.com>
2024-10-25 02:00:30 +0700

    Update Inventory.ts

--- src/Inventory.ts
+++ src/Inventory.ts
@@ -3,15 +3,15 @@ import { Product } from './Product';
 export class Inventory<T extends Product> {
     private items: T[] = [];
 
-    addItem(item: T): void {
+    public addItem(item: T): void {
         this.items.push(item);
     }
 
-    listItems(): T[] {
-        return [...this.items];
+    public listItems(): T[] {
+        return this.items;
     }
 
-    getItemCount(): number {
+    public getItemCount(): number {
         return this.items.length;
     }
 }
