bb7664b45ea72854330342a6cdc50636a8ec3353
FriskyXE <supanat.tinz@gmail.com>
2024-10-25 02:00:57 +0700

    Update PriceUtils.ts

--- src/PriceUtils.ts
+++ src/PriceUtils.ts
@@ -1,8 +1,9 @@
 export function createPriceMultiplier(multiplier: number): (price: number) => number {
     if (multiplier <= 0) {
-        throw new Error("Multiplier must be greater than 0");
+        console.error("Multiplier must be greater than 0");
+        return price => price;
     }
-    return (price: number) => price * multiplier;
+    return price => price * multiplier;
 }
 
 export const applyTax = createPriceMultiplier(1.07);
